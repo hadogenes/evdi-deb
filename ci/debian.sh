695d7e3242e7a2cbe9b2bd5d246d84d2955766ea
Jacek Szafarkiewicz <szafar@linux.pl>
2017-08-13 19:00:29 +0200

    Add dkms install

--- ci/debian.sh
+++ ci/debian.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
 apt update
-apt install -y build-essential devscripts debhelper wget
+apt install -y build-essential devscripts debhelper \
+    wget \
+    dkms
 
 cd base-src
 make
